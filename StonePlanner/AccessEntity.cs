ff125ca7dcae718355b0641801822883909411b6
Arabidopsis <MethodBox@163.com>
2024-07-06 09:41:04 +0800

    Aim Dev.24070601_0942
    1. 进一步完善了Access工具类、

--- StonePlanner/AccessEntity.cs
+++ StonePlanner/AccessEntity.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
+using System.Linq;
 using System.Text;
 using static StonePlanner.Interfaces;
 
 namespace StonePlanner
 {
     /// <summary>
-    /// Provide methods for serialization or deserialization objects for Access
+    /// Provide methods for serialization or deserialization objects for Access database
     /// </summary>
     internal class AccessEntity
     {
@@ -110,7 +111,7 @@ namespace StonePlanner
             string propertyValue, string tableName) where R : IMappingTable
         {
             string databaseColumnName;
-            if (mappingTable is null) databaseColumnName = propertyName;
+            if (mappingTable.GetType() == typeof(NonMappingTable)) databaseColumnName = propertyName;
             else databaseColumnName = mappingTable.GetColumnName(propertyName);
             var removeSring = $"delete from {tableName} where {databaseColumnName} = {propertyValue}";
             var removeCommand = _dbConnection.CreateCommand();
@@ -119,15 +120,77 @@ namespace StonePlanner
         }
 
         /// <summary>
-        /// Get object instance from database
+        /// Get specific object instance from database
         /// </summary>
         /// <typeparam name="T">Type of data source</typeparam>
         /// <typeparam name="R">Type of mapping table</typeparam>
-        /// <param name="mappingTable">Provide mapping from database row headers to field names</param>
+        /// <param name="mappingTable">Instance of mapping table</param>
         /// <param name="tableName">Name of data table</param>
-        /// <returns></returns>
-        internal IEnumerable<T> GetElement<T, R>(R mappingTable, string tableName)
+        /// <param name="propertyName">Name of specific property</param>
+        /// <param name="propertyValue">Value of specific property</param>
+        /// <param name="tableFirst">Whether search from table first</param>
+        /// <returns>Specific object instance list</returns>
+        internal List<T> GetElement<T, R>(R mappingTable, string tableName, 
+            string propertyName, string propertyValue ,bool tableFirst = true)
         where R : IMappingTable
+        {
+            string databaseColumnName;
+            List<T> result = new List<T>();
+
+            if (mappingTable.GetType() == typeof(NonMappingTable)) databaseColumnName = propertyName;
+            else databaseColumnName = mappingTable.GetColumnName(propertyName);
+
+            if (tableFirst)
+            {
+                var searchSring = $"select * from {tableName} where {databaseColumnName} = {propertyValue}";
+                var removeCommand = _dbConnection.CreateCommand();
+                removeCommand.CommandText = searchSring;
+                var reader = removeCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    T instance = (T) Activator.CreateInstance(typeof(T));
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        string fromColumnName = reader.GetName(i);
+                        string fromPropertyName;
+
+                        if (mappingTable.GetType() == typeof(NonMappingTable)) 
+                            fromPropertyName = fromColumnName;
+                        else 
+                            fromPropertyName = mappingTable.GetPropertyName(fromColumnName);
+
+                        var propertyInfo = instance.GetType().GetProperty(fromPropertyName);
+                        propertyInfo.SetValue(instance, reader[i]);
+                    }
+                    result.Add(instance);
+                }
+            }
+
+            else
+            {
+                List<T> allItems = GetElements<T,R>(tableName,mappingTable);
+                result = (List<T>) allItems.Where(obj =>
+                {
+                    var type = typeof(T);
+                    var propertyInfo = type.GetProperty(propertyName);
+                    return Convert.ToString(propertyInfo.GetValue(obj)) == propertyValue;
+                });
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get all object instance from database
+        /// </summary>
+        /// <typeparam name="T">Type of data source</typeparam>
+        /// <typeparam name="R">Type of mapping table</typeparam>
+        /// <param name="tableName">Name of data table</param>
+        /// <param name="mappingTable">Instance of mapping table</param>
+        /// <returns>Object instance list</returns>
+        internal List<T> GetElements<T,R>(string tableName,R mappingTable)
+            where R:IMappingTable
         {
             // Get all name of column
             var queryCommand = _dbConnection.CreateCommand();
@@ -170,5 +233,11 @@ namespace StonePlanner
             else databaseColumnName = mappingTable.GetColumnName(propertyName);
             var changeSring = $"delete from {tableName} where {databaseColumnName} = {propertyValue}";
         }
+    }
+
+    internal class NonMappingTable : IMappingTable
+    {
+        public string GetPropertyName(string _) => null;
+        public string GetColumnName(string _) => null;
     }
 }
